7dc3dccd8860b1619bee5575043aeb1537f8b390
Mikhail Akimov <rovinj.akimov@gmail.com>
2022-11-18 19:13:34 +0300

    make password secret, bump version, update deps

--- tests/test_dictum_backend_vertica.py
+++ tests/test_dictum_backend_vertica.py
@@ -1,11 +1,15 @@
+import inspect
 import shlex
 import subprocess
 
+from dictum_core.backends.secret import Secret
+
 from dictum_backend_vertica import __version__
+from dictum_backend_vertica.vertica_backend import VerticaBackend
 
 
 def test_version():
-    assert __version__ == "0.1.3"
+    assert __version__ == "0.1.4"
 
 
 def test_entry_point():
@@ -14,4 +18,11 @@ def test_entry_point():
             "python -c 'from dictum_core.backends.base import Backend; "
             'assert "vertica" in Backend.registry\''
         )
+    )
+
+
+def test_password_is_secret():
+    assert (
+        inspect.signature(VerticaBackend.__init__).parameters["password"].annotation
+        is Secret
     )
